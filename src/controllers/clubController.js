48d0ff77049a9ed0f7afa7578e5ab376f688f5ce
Francisco Zuidwijk <electrocisko@gmail.com>
2023-06-21 17:20:17 -0300

    Se agrego una validacion en controller para que no se repita el club en el array

--- src/controllers/clubController.js
+++ src/controllers/clubController.js
@@ -4,24 +4,28 @@ const randomClubId = () => {
   const max = 10;
   const min = 1;
   const idClub = Math.floor(Math.random() * (max - min + 1) + min);
-return idClub
-}
+  return idClub;
+};
 
-const getRandomClub = (req,res) => {
-  const ThreeClubs = [];   
-  let club; 
+const getRandomClub = (req, res) => {
+  const ThreeClubs = [];
+  let club;
   let idClub;
-  for(let i=0;i<3;i++) {
-    idClub = randomClubId()
-    club = clubes.find( (item) => item.id == idClub);
-    ThreeClubs.push(club)
-  }
- res.status(200).json({
-  clubes: ThreeClubs
- })
-}
+  let duplicate;
+  do {
+    idClub = randomClubId();
+    duplicate = ThreeClubs.findIndex((item) => item.id == idClub);
+    if (duplicate == -1) {
+      club = clubes.find((item) => item.id == idClub);
+      ThreeClubs.push(club);
+    }
+  } while (ThreeClubs.length < 3);
+
+  res.status(200).json({
+    clubes: ThreeClubs,
+  });
+};
 
 export const clubesControl = {
   getRandomClub,
-
 };
