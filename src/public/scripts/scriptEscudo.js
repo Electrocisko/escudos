76d3bbe3bcd0c83d6872a3e3e50034ad4fbca363
Francisco Zuidwijk <electrocisko@gmail.com>
2023-06-23 10:59:49 -0300

    trabajando sobre como se muestra el orden aleatorio en los botones

--- src/public/scripts/scriptEscudo.js
+++ src/public/scripts/scriptEscudo.js
@@ -2,14 +2,14 @@ const btn1 = document.getElementById("btn_1");
 const btn2 = document.getElementById("btn_2");
 const btn3 = document.getElementById("btn_3");
 const imagelogo = document.getElementById("image-logo");
-const buttons = document.getElementsByTagName('button');
-const logo = document.getElementById('logo')
+const buttons = document.getElementsByTagName("button");
+const logo = document.getElementById("logo");
 
 let randomClub;
 let counter = 0;
 let points = 0;
 
-const randomIndexClub = (max,min) => {
+const randomIndexClub = (max, min) => {
   const index = Math.floor(Math.random() * (max - min + 1) + min);
   return index;
 };
@@ -20,8 +20,8 @@ const checkClub = (e) => {
     Toastify({
       text: "Correcto!!",
       offset: {
-        x: 300, 
-        y: 20, 
+        x: 300,
+        y: 20,
       },
     }).showToast();
     points++;
@@ -29,8 +29,8 @@ const checkClub = (e) => {
     Toastify({
       text: "MAL !!!!",
       offset: {
-        x: 300, 
-        y: 20, 
+        x: 300,
+        y: 20,
       },
       style: {
         background: "linear-gradient(to right, #ff6242, #ff4122)",
@@ -41,7 +41,6 @@ const checkClub = (e) => {
   if (counter < 10) {
     listClubes();
   } else {
-    
     Swal.fire({
       title: "Game Over",
       text: `Respondiste ${points} veces bien `,
@@ -55,49 +54,42 @@ const checkClub = (e) => {
   }
 };
 
-btn1.addEventListener('click', (e) => {
-  checkClub(e)
+btn1.addEventListener("click", (e) => {
+  checkClub(e);
 });
-btn2.addEventListener('click', (e) => {
-  checkClub(e)
-})
-btn3.addEventListener('click', (e) => {
-  checkClub(e)
-})
+btn2.addEventListener("click", (e) => {
+  checkClub(e);
+});
+btn3.addEventListener("click", (e) => {
+  checkClub(e);
+});
 
-logo.addEventListener('click', () => {
-  listClubes();
-}, {once: true})
-
+logo.addEventListener(
+  "click",
+  () => {
+    listClubes();
+  },
+  { once: true }
+);
 
 const listClubes = async () => {
   const response = await fetch("/api/club");
   const data = await response.json();
   imagelogo.src = data.clubes[0].imageUrl;
   randomClub = data.clubes[0];
-  let shufleList=[];
+  let shufleList = [];
   let originalList = data.clubes;
   let max = 2;
   let min = 0;
   let i;
-  
 
   do {
-   i =  randomIndexClub(max,min);
-   let sliced = originalList.splice(i,1);
-   shufleList.push(sliced);
-    max = max-1
+    i = randomIndexClub(max, min);
+    let sliced = originalList.splice(i, 1);
+    shufleList.push(sliced[0]);
+    max = max - 1;
   } while (originalList.length > 0);
-
-  console.log('shufleList', shufleList[0][0].name)
-  console.log('shufleList', shufleList[1][0])
-  console.log('shufleList', shufleList[2])
-
-  btn1.innerText = shufleList[0][0].name;
-  btn2.innerText = shufleList[1][0].name;
-  btn3.innerText = shufleList[2][0].name;
-
- 
+  btn1.innerText = shufleList[0].name;
+  btn2.innerText = shufleList[1].name;
+  btn3.innerText = shufleList[2].name;
 };
-
-
